cb43269ba0685add7689d565352034cfd3e67957
HamsterHat <ccrrow@yandex.ru>
2026-09-18 23:52:56 +0300

    Welcome to Java, babies!

---
+++ src/dights/WoD.java
@@ -0,0 +1,19 @@
+package dights;
+
+import arc.Events;
+import mindustry.game.EventType;
+import mindustry.mod.*;
+
+
+
+public class WoD extends Mod{
+    public WoD(){
+        // no
+    }
+
+
+    @Override
+    public void loadContent(){
+        //bruhh
+    }
+}
